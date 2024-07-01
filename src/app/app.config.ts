73821a3595b6d90bbb6f4fcb004b382ad664a356
Julia <julia.naczke007@gmail.com>
2024-07-01 10:25:29 +0200

    firebase setup and CLI installation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"angular-firebase-project-994c9","appId":"1:320270123720:web:0cae3a695352ac176cdbfd","storageBucket":"angular-firebase-project-994c9.appspot.com","apiKey":"AIzaSyDyLN6oaRep1CBE6tjYIpkkailujaOKaeA","authDomain":"angular-firebase-project-994c9.firebaseapp.com","messagingSenderId":"320270123720"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDyLN6oaRep1CBE6tjYIpkkailujaOKaeA",
+    authDomain: "angular-firebase-project-994c9.firebaseapp.com",
+    projectId: "angular-firebase-project-994c9",
+    storageBucket: "angular-firebase-project-994c9.appspot.com",
+    messagingSenderId: "320270123720",
+    appId: "1:320270123720:web:0cae3a695352ac176cdbfd"
+  },
+};
